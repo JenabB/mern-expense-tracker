f20911718a4038c8dccb3bbd990176fa47ff9328
wadahkode <mvp.dedefilaras@gmail.com>
2021-07-14 20:50:32 +0700

    fix for path to string in res.sendFile(path, ...)

--- server.js
+++ server.js
@@ -23,9 +23,29 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.use("/api/v1/transactions", transactions);
-  app.get("*", (req, res) => 
-    res.sendFile(path.toString(), path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  // app.get("*", (req, res) => 
+  //   res.sendFile(path, path.resolve(__dirname, "client", "build", "index.html"))
+  // );
+  app.get("*", (req,res,next) => {
+    const options = {
+      root: path.join(__dirname, 'client/build'),
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    };
+
+    let filePath = req.path;
+
+    res.sendfile(filePath, options, (err) => {
+      if (err) {
+        next(err);
+      } else {
+        console.log('Sent:', filePath);
+      }
+    });
+  })
 }
 
 app.use("/api/v1/transactions", transactions);
