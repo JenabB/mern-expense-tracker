2b4fe404e4addd9ce4ef88b7c5dcb25614374c07
wadahkode <mvp.dedefilaras@gmail.com>
2021-07-14 20:11:23 +0700

    fix error resolve not defined

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const colors = require('colors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 connectDB();
-const transactions = require('./routes/transactions');
+const transactions = require("./routes/transactions");
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.use('/api/v1/transactions', transactions);
-  app.get('*', (req, res) =>
-    res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.use("/api/v1/transactions", transactions);
+  app.get("*", (req, res) =>
+    res.sendFile(path, path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
-app.use('/api/v1/transactions', transactions);
+app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
