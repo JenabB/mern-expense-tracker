a0db318fff06dd6fd879560c1a0058dc9be03013
Ziva <birulangit1996@gmail.com>
2021-07-14 16:35:00 +0700

    a

--- client/src/components/Balance.js
+++ client/src/components/Balance.js
@@ -12,13 +12,7 @@ export const Balance = () => {
     <div className="bg-indigo-900 w-full p-10 pb-20 text-white">
       <h4>Your Balance</h4>
       <h1 className="text-2xl font-bold">Rp {numberWithCommas(total)}</h1>
-      <div className="mt-4">
-        {total <= 0 ? (
-          <h1>Harus bisa atur keuangan ya :)</h1>
-        ) : (
-          <h1>Semangat atur keuangannya :)</h1>
-        )}
-      </div>
+      <div className="mt-4"></div>
     </div>
   );
 };
